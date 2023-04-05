ee3fbb6591e2f2940f242aeb43ab543e88daaa30
Carson O'Sullivan <66914659+yoshirozay@users.noreply.github.com>
2023-04-05 10:50:39 -0400

    Initial Commit

---
+++ MusicApp/MusicAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicAppApp.swift
+//  MusicApp
+//
+//  Created by Carson O'Sullivan on 4/5/23.
+//
+
+import SwiftUI
+
+@main
+struct MusicAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
